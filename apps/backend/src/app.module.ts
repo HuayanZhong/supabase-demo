9b1fea189e0c4a73ccebf24a24d0bd8c3b673356
花颜 <2870690469@qq.com>
2026-07-13 20:56:21 +0800

    feat(backend): 新增 locations 模块基础脚手架
    新增地点模块的实体、DTO、控制器、服务以及模块定义，并将其注册到根模块中

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { WeathersModule } from "./modules/weathers/weathers.module";
 import { QuotesModule } from "./modules/quotes/quotes.module";
 import { LoggerModule } from "nestjs-pino";
 import { pinoConfig } from "./config/pino";
+import { LocationsModule } from "./modules/locations/locations.module";
 
 @Module({
-  imports: [WeathersModule, QuotesModule, LoggerModule.forRoot(pinoConfig)],
+  imports: [WeathersModule, QuotesModule, LoggerModule.forRoot(pinoConfig), LocationsModule],
   controllers: [],
   providers: [],
 })
